2a58dca6990ea582e8fefcdf28b5e7763ce16663
Akaike <akaike@live.com>
2024-12-31 02:49:22 +0100

    feat: add cargo-generate template support
    - Add cargo-generate.toml with database and JWT configuration prompts
    - Update README with cargo-generate installation instructions

--- server/src/router/todo.rs
+++ server/src/router/todo.rs
@@ -1,14 +1,18 @@
 use axum::{
+    middleware,
     routing::{get, post},
     Router,
 };
 
-use crate::{handler::todo, state::app::AppState};
+use crate::{handler::todo, middleware::auth::is_authenticated, state::app::AppState};
 
 pub fn init() -> Router<AppState> {
-    Router::new()
+    let router = Router::new()
         .route("/:todo_id", get(todo::get_by_id).put(todo::update))
-        .route("/", post(todo::create))
-    // middleware jwt auth example
-    //.route_layer(middleware::from_fn(is_authenticated))
+        .route("/", post(todo::create));
+
+    // Uncomment to enable JWT authentication for all routes in this router
+    // router.layer(middleware::from_fn(is_authenticated));
+
+    router
 }
