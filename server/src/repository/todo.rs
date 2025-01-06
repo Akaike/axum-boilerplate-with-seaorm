8186592cdb5019037b343a9ef77a4f2a0a69e497
Akaike <akaike@live.com>
2025-01-06 21:08:50 +0100

    refactor(todo): update TodoRepository to use ActiveModel directly
    - Simplified the create method by directly returning the result of get_by_id after insertion.

--- server/src/repository/todo.rs
+++ server/src/repository/todo.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Utc};
-use entity::todo::{self, Entity as TodoEntity, Model};
+use entity::todo::{self, ActiveModel, Entity as TodoEntity, Model};
 use sea_orm::{DatabaseConnection, EntityTrait, Set};
 use uuid::Uuid;
 
@@ -9,14 +9,9 @@ use crate::error::DbError;
 
 #[async_trait]
 pub trait TodoRepository: Send + Sync {
-    async fn get_by_id(&self, id: Uuid) -> Result<todo::Model, DbError>;
-    async fn create(&self, title: String) -> Result<todo::Model, DbError>;
-    async fn update(
-        &self,
-        id: Uuid,
-        title: String,
-        completed: bool,
-    ) -> Result<todo::Model, DbError>;
+    async fn get_by_id(&self, id: Uuid) -> Result<Model, DbError>;
+    async fn create(&self, title: String) -> Result<Model, DbError>;
+    async fn update(&self, id: Uuid, title: String, completed: bool) -> Result<Model, DbError>;
     async fn delete(&self, id: Uuid) -> Result<(), DbError>;
 }
 
@@ -37,7 +32,7 @@ impl TodoRepository for TodoRepositoryImpl {
     async fn create(&self, title: String) -> Result<Model, DbError> {
         let date_time_now = DateTime::from(Utc::now());
 
-        let new_todo = todo::ActiveModel {
+        let new_todo = ActiveModel {
             id: Set(Uuid::new_v4()),
             title: Set(title),
             completed: Set(false),
@@ -46,17 +41,15 @@ impl TodoRepository for TodoRepositoryImpl {
         };
 
         let res = TodoEntity::insert(new_todo).exec(&self.db).await?;
-        TodoEntity::find_by_id(res.last_insert_id)
-            .one(&self.db)
-            .await
-            .map(|opt| opt.ok_or(DbError::NotFound))?
+
+        Ok(self.get_by_id(res.last_insert_id).await?)
     }
 
     async fn update(&self, id: Uuid, title: String, completed: bool) -> Result<Model, DbError> {
         let todo_found_result = TodoEntity::find_by_id(id).one(&self.db).await?;
         let original_todo = todo_found_result.ok_or(DbError::NotFound)?;
 
-        let updated_todo = todo::ActiveModel {
+        let updated_todo = ActiveModel {
             id: Set(id),
             title: Set(title),
             completed: Set(completed),
@@ -64,7 +57,7 @@ impl TodoRepository for TodoRepositoryImpl {
             updated_at: Set(DateTime::from(Utc::now())),
         };
 
-        Ok(TodoEntity::update(updated_todo).exec(&self.db).await?);
+        Ok(TodoEntity::update(updated_todo).exec(&self.db).await?)
     }
 
     async fn delete(&self, id: Uuid) -> Result<(), DbError> {
